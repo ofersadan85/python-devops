dfb50598e2efe3aa83839011e0fea1d1e504f746
Ofer Sadan <ofersadan85@gmail.com>
2025-02-11 12:12:21 +0200

    fastapi demo with httpx

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from log import setup_logging
+from dataclasses import dataclass
+from fastapi import FastAPI
+import httpx
 
-logger = setup_logging()
-servers = {"nginX": True, "DoCkEr": False}
-
-
-def get_server_status_2(server_name: str) -> bool:
-    lowercase_servers = {key.strip().lower(): value for key, value in servers.items()}
-    if server_name in lowercase_servers:
-        return lowercase_servers[server_name]
-    else:
-        logger.error(f"The server name {server_name} does not exist")
+app = FastAPI()
 
 
-def get_server_status(server_name: str) -> bool:
-    lowercase_servers = {key.strip().lower(): value for key, value in servers.items()}
-    try:
-        return lowercase_servers[server_name]
-    except KeyError:
-        logger.error(f"The server name {server_name} does not exist")
+@dataclass
+class User:
+    name: str
+    email: str
 
 
-while True:
-    server_name = input("Enter server name: ").strip().lower()
-    status = get_server_status(server_name)
-    logger.info(f"Server {server_name} status is: {status}")
+@app.get("/")
+def hello_world():
+    "This is our main function"
+    return {"result": ["Hello World", 1, 2, True, None], "error": False}
+
+
+@app.get("/users")
+def get_users() -> list[User]:
+    response = httpx.get("https://jsonplaceholder.typicode.com/users")
+    users = response.json()
+    return users
+
+
+@app.post("/users")
+def create_user(new_user: User) -> bool:
+    return True
